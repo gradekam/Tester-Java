b3044d173cf7b4767b58ad75a87a7962fc02e4d8
gradekam <gradek.a.m@gmail.com>
2022-02-13 14:39:52 +0100

    może

--- src/DemoApp.java
+++ src/DemoApp.java
@@ -1,6 +1,6 @@
 public class DemoApp {
     public static void main (String[] args) {
-        System.out.print("hello world");
+        System.out.print("hello world Im trying for 2h already to push you");
     }
 
 }
