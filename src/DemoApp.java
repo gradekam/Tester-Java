c7d47319cca6ba295f41f224bdc5c198b8513733
gradekam <gradek.a.m@gmail.com>
2022-02-13 15:41:17 +0100

    zmienne

--- src/DemoApp.java
+++ src/DemoApp.java
@@ -14,6 +14,26 @@ public class DemoApp {
         System.out.println(true);
         System.out.println(false);
 
+        //zmienne
+        int number10 = 10;
+        System.out.println(number10);
+
+        double numbercoma = 10.6;
+        System.out.println(numbercoma);
+
+        String someText = "bla";
+        System.out.println(someText);
+
+        char someChar = 's';
+        System.out.println(someChar);
+
+        boolean trueValue = true;
+        System.out.println(trueValue);
+
+        boolean falseValue = false;
+        System.out.println(falseValue);
+
+
 
     }
 
