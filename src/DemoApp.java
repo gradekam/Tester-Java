5a176c9fa6db3fedd6f7a9c356b1303d9d2a2398
gradekam <gradek.a.m@gmail.com>
2022-02-13 15:12:21 +0100

    added comments

--- src/DemoApp.java
+++ src/DemoApp.java
@@ -1,5 +1,9 @@
 public class DemoApp {
     public static void main (String[] args) {
+        //start aplikacji
+        //psmv
+        //sout
+
         System.out.print("hello world Im trying for 2h already to push you");
     }
 
