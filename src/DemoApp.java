92abadbb32a4dcb43ef026752a272ca1dd4b726d
gradekam <gradek.a.m@gmail.com>
2022-02-13 15:20:56 +0100

    rodzaje danych + skróty

--- src/DemoApp.java
+++ src/DemoApp.java
@@ -3,8 +3,18 @@ public class DemoApp {
         //start aplikacji
         //psmv
         //sout
+        //to copy a code ctrl D on a line to repeat it
+        // to comment the line control + /
 
+        //typy danych
         System.out.print("hello world Im trying for 2h already to push you");
+        System.out.print(10);
+        System.out.print(10.6);
+        System.out.print('a');
+        System.out.print(true);
+        System.out.print(false);
+
+
     }
 
 }
